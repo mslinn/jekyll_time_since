dde2e7fb08f504376409f48d26afbd8ddb2c0c83
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-28 08:25:52 -0400

    Works

--- spec/jekyll_block_tag_plugin_template.rb
+++ spec/jekyll_block_tag_plugin_template.rb
@@ -8,11 +8,12 @@ require_relative "../lib/jekyll_block_tag_plugin_template"
 RSpec.describe(JekyllPluginBlockTagTemplate) do
   include JekyllPluginBlockTagTemplate
 
-  argv = Shellwords.split "param1=value1 param2='value2' param3=\"value3\""
+  argv = Shellwords.split "param0 param1=value1 param2='value2' param3=\"value3\""
   parser = KeyValueParser.new
 
   it "parses arguments" do
     options = parser.parse(argv)
+    expect(options[:param0]).to eq(true)
     expect(options[:param1]).to eq("value1")
     expect(options[:param2]).to eq("value2")
     expect(options[:param3]).to eq("value3")
