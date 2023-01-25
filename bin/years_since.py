615b7375604f7af9ed6cad2a589c95d80f20acfa
Mike Slinn <mslinn@mslinn.com>
2023-01-25 16:17:11 -0500

    -

--- bin/years_since.py
+++ bin/years_since.py
@@ -4,13 +4,16 @@ from dateutil.parser import parse
 
 # pip install python-dateutil
 
-def years_since(day):
-    today = datetime.date.today()
-    years = today.year - day.year
-    if today.month < day.month or (today.month == day.month and today.day < day.day):
+def years_since(datetime):
+    today = datetime.date().today()
+    years = today.year - datetime.year
+    if today.month < datetime.month or (today.month == datetime.month and today.day < datetime.day):
         years -= 1
     return years
 
-date = datetime.date(parse(sys.argv[1]))
-result = years_since(date)
+if len(sys.argv)==1:
+  sys.exit("Error: A date must be provided.")
+
+datetime = parse(sys.argv[1])
+result = years_since(datetime)
 print(result)
