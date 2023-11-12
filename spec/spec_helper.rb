c2ced0ef14b36f3f05754eef9d13449de7f797b7
Mike Slinn <mslinn@mslinn.com>
2023-11-11 21:22:18 -0500

    Brought up to current standards

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,8 @@ require_relative "../lib/jekyll_time_since"
 Jekyll.logger.log_level = :info
 
 RSpec.configure do |config|
-  config.filter_run :focus
+  config.filter_run_when_matching focus: true
   config.order = "random"
-  config.run_all_when_everything_filtered = true
 
   # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
   config.example_status_persistence_file_path = "spec/status_persistence.txt"
